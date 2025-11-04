3def5f7527225011b55e42d177b3214b73b4f657
nicko89 <71294595+nicko89@users.noreply.github.com>
2025-11-04 17:14:03 -0500

    App.py

---
+++ App.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title = "Provision Cartera USA", layout="wide")
+
+@st.cache_data
+def cargar_datos()
+    df = pd.read_excel(Data/Base Provision.xlsx)
+    return df
+
+df = cargar_datos()
+
+st.title("Provision Cartera USA")
+st.subheader("Vista general de los datos")
+St.dataframe(df.head())
